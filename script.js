b6ba26263e3bdbc3753c304867e5a3b4d7e3551b
KaranSurana <techtrends288@gmail.com>
2021-09-17 16:19:28 +0530

    Commit

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ function myFunction1(){
 }
 bg.addEventListener("mouseover", myFunction);
 bg.addEventListener("mouseout", myFunction1);
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
